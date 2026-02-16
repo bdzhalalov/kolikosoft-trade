3608f38fb75af43b0c1b7986d1eee524a3dc8fca
bahadzhalalov <bahadzhalalov@gmail.com>
2026-02-16 19:58:21 +0500

    add item domain init and graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,15 +2,55 @@ package server
 
 import (
 	"context"
+	"errors"
+	"github.com/bdzhalalov/kolikosoft-trade/internal/item"
 	"github.com/bdzhalalov/kolikosoft-trade/pkg/config"
 	"github.com/bdzhalalov/kolikosoft-trade/pkg/database"
 	"github.com/bdzhalalov/kolikosoft-trade/pkg/logger"
+	"net/http"
+	"time"
 )
 
 func Start(ctx context.Context, config *config.Config) {
-	logger := logger.Logger(config)
+	log := logger.Logger(config)
 
 	db := database.ConnectToDB(ctx, config)
-	defer db.Close()
 
+	httpClient := &http.Client{}
+	skinPortClient := item.NewSkinPortClient(httpClient, config.SkinPortBaseURL)
+	itemService := item.NewService(skinPortClient, log)
+
+	itemHandler := item.NewHandler(itemService)
+
+	router := Router(itemHandler)
+
+	apiServer := &http.Server{
+		Addr:    config.Addr,
+		Handler: router,
+	}
+
+	errCh := make(chan error, 1)
+
+	go func() {
+		log.Infof("Running API server on port: %s", config.Addr)
+
+		errCh <- apiServer.ListenAndServe()
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Info("Shutting down API server...")
+	case err := <-errCh:
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.WithError(err).Error("API server stopped unexpectedly")
+		}
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_ = apiServer.Shutdown(shutdownCtx)
+	_ = db.Close()
+
+	log.Info("API server shutdown complete")
 }
